c0e8c7792983ee87dfda99166453bbc35755afe1
smalldatabrains <smalldatabrains@gmail.com>
2025-08-12 10:58:44 +0200

    drawing function skeletong

--- frontend/src/components/WhiteBoard.jsx
+++ frontend/src/components/WhiteBoard.jsx
@@ -2,15 +2,29 @@ import { useState } from "react";
 
 const WhiteBoardCanvas = () => {
 
+    const startDrawing = () => {
+        console.log("start Drawing")
+    }
+
+    const draw = () => {
+        console.log("Drawing")
+    }
+
+    const stopDrawing = () => {
+        console.log("strop Drawing")
+    }
 
     return (
         <canvas 
             width={800} 
             height={400}
-            style={{border: "1px solid black"}}     
+            style={{border: "1px solid black"}}
+            onMouseDown={startDrawing}
+            onMouseMove={draw}
+            onMouseUp={stopDrawing}
+            onMouseLeave={stopDrawing} 
         />
     )
 }
-
 
 export default WhiteBoardCanvas;
