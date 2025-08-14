688517b783cfe538ed2372634c057537980db68a
smalldatabrains <smalldatabrains@gmail.com>
2025-08-14 08:31:42 +0000

    Dataset class and more complex nn

--- pytorch_study.py
+++ pytorch_study.py
@@ -1,11 +1,28 @@
 from torch import nn
 import torch
 
+from torch.utils.data import DataLoader, Dataset
+
+class CustomDataset(Dataset):
+    def __init__(self, input, label, transform=None, target_transform=None):
+        self.input=input
+        self.label=label
+
+    def __len__(self):
+        return len(self.label)
+
+    def __getitem__(self, index):
+        item = 'hello'
+        return item
+
+
 class Classifier(nn.Module)
     def __init__(self):
         super(Classifier, self).__init__()
         self.classifier = nn.Sequential(
-            nn.Linear(4,1),
+            nn.Linear(10,5),
+            nn.Sigmoid(),
+            nn.Linear(5,1),
             nn.Sigmoid()
     )
 
@@ -18,15 +35,18 @@ linear_layer = model.classifier[0]
 
 print("Weight", linear_layer.weight)
 print("Bias", linear_layer.bias)
-tensor = torch.rand([2,4])
+input = torch.rand([1000,10]) #1000 examples (N), 4 features (F)
+label = torch.rand([1000,1])
 
-print(tensor)
+print("There are",len(label),"examples in the dataset")
 
-print(tensor.dtype)
-print(tensor.shape)
-print(tensor.device)
+print(input)
 
-output = model.forward(tensor)
+print(input.dtype)
+print(input.shape)
+print(input.device)
+
+output = model.forward(input)
 
 print(output.dtype)
 print(output.shape)
