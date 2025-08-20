b809b839f9b0164c9eedeafeb0029ba6174b34fe
smalldatabrains <smalldatabrains@gmail.com>
2025-08-20 13:18:53 +0000

    websocket server update

--- backend/server.py
+++ backend/server.py
@@ -8,8 +8,11 @@ clients = set() #do not allow duplicate values, list of clients on the server
 async def handler(websocket):
     # Register
     clients.add(websocket)
+    print("There are", len(clients), "clients on the server")
     try:
         async for message in websocket:
+            print(message)
+            print(websocket.remote_address) # ip and port
             # broadcast to everyone else:
         for client in clients:
             if client != websocket:
@@ -19,3 +22,11 @@ async def handler(websocket):
             pass
     finally:
         clients.remove(websocket)
+
+async def main():
+    async with websockets.serve(handler, "127.0.0.1", 8765) #localhost
+        print("Websocket server online")
+        await asyncio.Future() #run forever
+
+if __name__ == "__main__":
+    asyncio.run(main())
