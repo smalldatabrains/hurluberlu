751604f8271274020295d0c8d58a3f01932f1143
smalldatabrains <smalldatabrains@gmail.com>
2025-10-09 13:07:30 +0000

    funcitonnal dfs

--- graph_search.py
+++ graph_search.py
@@ -7,12 +7,18 @@ graph = {
     'F': []
 }
 
+# deep first search, iterative version
+
 def dfs(graph, start):
-    stack=[start]
+    stack=[start] # stack logic is LAST IN FIRST OUT
     visited = []
 
     while stack:
         node = stack.pop()
         if node not in visited:
             visited.append(node)
-            
+            stack.extend(graph[node])
+    return visited
+
+print(dfs(graph,'A'))
+
